b1397c634b0c31bf07b30f00137b18a58067d924
Chris Marshall <chris@riftvalleysoftware.com>
2019-03-21 15:16:31 -0400

    Initial Commit

---
+++ RVS_ParseXMLDuration/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  RVS_ParseXMLDuration
+//
+//  Created by Chris Marshall on 3/21/19.
+//  Copyright © 2019 The Great Rift Valley Software Company. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
