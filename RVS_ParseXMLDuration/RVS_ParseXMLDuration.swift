0b5669fbd18df71ef2da9c5d4afa6a9c0892ce10
Chris Marshall <chris@riftvalleysoftware.com>
2019-03-22 10:54:52 -0400

    I think that I have everything working, and I'm starting to apply polish to the turd...

--- RVS_ParseXMLDuration/RVS_ParseXMLDuration.swift
+++ RVS_ParseXMLDuration/RVS_ParseXMLDuration.swift
@@ -39,7 +39,7 @@ public extension String {
         /* ############################################################## */
         /**
          */
-        func scanString(_ inString: String!, isNegative: Bool = false, isDate: Bool = false) -> DateComponents! {
+        func _asXMLDurationScanString(_ inString: String!, isNegative: Bool = false, isDate: Bool = false) -> DateComponents! {
             var ret: DateComponents!
             
             if let parseTarget = inString {
@@ -47,7 +47,6 @@ public extension String {
                 let valueIndicators = CharacterSet(charactersIn: "YMDHS")   // These are the value indicators (years, months/minutes, days, hours, seconds)
                 let scanner = Scanner(string: parseTarget)  // Set up a scanner, with the string supplied.
                 scanner.charactersToBeSkipped = numbers.union(valueIndicators).inverted  // Ignore everything but what we give you.
-                ret = DateComponents()              // This is what we'll return (unless there's an error).
                 while !scanner.isAtEnd {            // Keep going until the end.
                     var value: NSString?            // The numerical value that will be scanned. We use NSString for the scanner.
                     var typeIndicator: NSString?    // This is the value indicator.
@@ -57,26 +56,32 @@ public extension String {
                     
                     // Quick validation and unwrapping.
                     if let value = value as String?, let doubleVal = Double(value), let typeIndicator = typeIndicator as String? {
-                        if floor(doubleVal) != doubleVal && "S" != typeIndicator {  // Only seconds can have a decimal value.
+                        if (isDate && "HS".contains(typeIndicator))                         // Any time values must come after "T".
+                            || (floor(doubleVal) != doubleVal && "S" != typeIndicator) {    // Only seconds can have a decimal value.
                             return nil
                         }
+                        
+                        if nil == ret {
+                            ret = DateComponents()                      // This is what we'll return (unless there's an error).
+                        }
                         let multiplier: Double = isNegative ? -1 : 1    // This is used to make values negative. We parse an absval.
-                        switch typeIndicator {  // Which components are set depends upon which value indicator we got.
+                        let intVal = Int(doubleVal * multiplier)
+                        switch typeIndicator {                          // Which components are set depends upon which value indicator we got.
                         case "Y":   // Years
-                            ret.year = Int(doubleVal * multiplier)
+                            ret.year = intVal
                         case "M":   // Months or Minutes
-                           if isDate {  // Which one depends on whether this is a date or a time.
-                                ret.month = Int(doubleVal * multiplier)
+                            if isDate {  // Which one depends on whether this is a date or a time.
+                                ret.month = intVal
                             } else {
-                                ret.minute = Int(doubleVal * multiplier)
+                                ret.minute = intVal
                             }
                         case "D":   // Days
-                            ret.day = Int(doubleVal * multiplier)
+                            ret.day = intVal
                         case "H":   // Hours
-                            ret.hour = Int(doubleVal * multiplier)
+                            ret.hour = intVal
                         case "S":   // Seconds and Nanoseconds. Fractions of seconds will result in a second property of zero.
-                            ret.second = Int(doubleVal * multiplier)
-                            let nanosecond = (doubleVal - floor(doubleVal)) * 1000000000  // The fractional part of the value.
+                            let nanosecond = (doubleVal - floor(doubleVal)) * 10000000000  // The fractional part of the value.
+                            ret.second = intVal
                             if 0 < nanosecond { // We only supply a nanosecond value if we actually have a fractional part.
                                 ret.nanosecond = Int(nanosecond * multiplier)
                             }
@@ -92,7 +97,7 @@ public extension String {
             
             return ret
         }
-        
+
         var target = self
         let isNegative = "-" == target.first
         if isNegative {
@@ -100,13 +105,16 @@ public extension String {
         }
         
         let timeDate = target.uppercased().components(separatedBy: "T")
-        if 1 == timeDate.count || !(1 < timeDate.count && timeDate[1].isEmpty) {
+        if 0 < timeDate.count,
+            !(1 < timeDate.count && timeDate[1].isEmpty),
+            "P" == timeDate[0].prefix(1),
+            "P-" != timeDate[0].prefix(2) {
             let dateString = "P" != timeDate[0] ? String(timeDate[0].dropFirst()) : nil
             let timeString = ((1 < timeDate.count) && !timeDate[1].isEmpty) ? timeDate[1] : nil
             
-            var returnValue = scanString(dateString, isNegative: isNegative, isDate: true)
+            var returnValue = _asXMLDurationScanString(dateString, isNegative: isNegative, isDate: true)
 
-            if let timeComp = scanString(timeString, isNegative: isNegative) {
+            if let timeComp = _asXMLDurationScanString(timeString, isNegative: isNegative) {
                 if nil != returnValue {
                     returnValue?.hour = timeComp.hour
                     returnValue?.minute = timeComp.minute
@@ -118,6 +126,73 @@ public extension String {
             }
             
             return returnValue
+        }
+        
+        return nil
+    }
+}
+
+/* ################################################################################################################################## */
+/**
+ This extension provides the reverse of the above.
+ 
+ You can have an instance of DateComponents express itself as a legal, optimized xs:duration String.
+ */
+public extension DateComponents {
+    /* ################################################################## */
+    /**
+     */
+    var asXMLDuration: String! {
+        // First, extract all of the components we're interested in. We either have a value, or 0.
+        let componentArray: [Int] = [
+            self.year ?? 0,
+            self.month ?? 0,
+            self.day ?? 0,
+            self.hour ?? 0,
+            self.minute ?? 0,
+            self.second ?? 0,
+            self.nanosecond ?? 0
+        ]
+        
+        // Now, if one is negative, they must ALL be negative. -1 is negative, 1 is positive, and -2 or 0 (no valid values) is error.
+        let multiplier = componentArray.reduce(0, { (current, nextItem) -> Int in
+            if -2 != current {  // Are we BORK?
+                if (nextItem < 0 && -1 == current) || (nextItem > 0 && 1 == current) {  // We assume that we are on the same page as the previous value.
+                    return current
+                } else if 0 == current {    // If this is the first valid value, then we set the agenda.
+                    return nextItem < 0 ? -1 : 1
+                } else {        // BORK
+                    return -2
+                }
+            } else {
+                return current  // BORK
+            }
+        })
+        
+        if 1 == abs(multiplier) {                               // -2 means we have an illegal situation (some are negative, and some are not). 0 means that we have no valid numbers.
+            // This allows us to loop through.
+            let tagArray: [String] = ["Y", "M", "D", "H", "M"]
+            
+            let components = componentArray.map(abs)            // Remove any negative influences. Feng Shui...
+            var resultString = -1 == multiplier ? "-P" : "P"    // Start your engines...
+            
+            // The first lot are easy and straightforward.
+            for index in 0..<tagArray.count {
+                if 3 == index {
+                    resultString += "T" // Time for time...
+                }
+                if 0 != components[index] {
+                    resultString += String(components[index]) + tagArray[index]
+                }
+            }
+            
+            // Seconds takes a bit more work, as it could be fractional.
+            if 0 != components[tagArray.count] || 0 != components[tagArray.count + 1] {
+                let value: Double = Double(components[tagArray.count]) + (Double(components[tagArray.count + 1]) / 10000000000)
+                resultString += String(value) + "S"
+            }
+            
+            return resultString
         }
         
         return nil
--- RVS_ParseXMLDurationTests/RVS_ParseXMLDurationTests.swift
+++ RVS_ParseXMLDurationTests/RVS_ParseXMLDurationTests.swift
@@ -9,9 +9,10 @@
 import XCTest
 
 class RVS_ParseXMLDurationTests: XCTestCase {
-    typealias TestTuple = (targetString: String, expectedResult: DateComponents?)
-    
-    static let testSequence: [TestTuple] = [
+    typealias FromStringTestTuple = (targetString: String, expectedResult: DateComponents?)
+    typealias FromComponentsTestTuple = (targetComponents: DateComponents, expectedResult: String?)
+
+    static let fromStringTests: [FromStringTestTuple] = [
         (targetString: "P2Y6M5DT12H35M30S", expectedResult: DateComponents(year: 2, month: 6, day: 5, hour: 12, minute: 35, second: 30)),
         (targetString: "P1DT2H", expectedResult: DateComponents(day: 1, hour: 2)),
         (targetString: "P20M", expectedResult: DateComponents(month: 20)),
@@ -19,7 +20,7 @@ class RVS_ParseXMLDurationTests: XCTestCase {
         (targetString: "P0Y20M0D", expectedResult: DateComponents(year: 0, month: 20, day: 0)),
         (targetString: "P0Y", expectedResult: DateComponents(year: 0)),
         (targetString: "-P60D", expectedResult: DateComponents(day: -60)),
-        (targetString: "PT1M30.5S", expectedResult: DateComponents(minute: 1, second: 30, nanosecond: 500000000)),
+        (targetString: "PT1M30.5S", expectedResult: DateComponents(minute: 1, second: 30, nanosecond: 5000000000)),
         (targetString: "P1Y2M3DT10H30M", expectedResult: DateComponents(year: 1, month: 2, day: 3, hour: 10, minute: 30)),
         (targetString: "-P120D", expectedResult: DateComponents(day: -120)),
         (targetString: "P1347Y", expectedResult: DateComponents(year: 1347)),
@@ -31,13 +32,12 @@ class RVS_ParseXMLDurationTests: XCTestCase {
         (targetString: "P1D", expectedResult: DateComponents(day: 1)),
         (targetString: "PT10H5s", expectedResult: DateComponents(hour: 10, second: 5)),
         (targetString: "P5Y11M", expectedResult: DateComponents(year: 5, month: 11)),
-        (targetString: "P2M4DT10M5.23S", expectedResult: DateComponents(month: 2, day: 4, minute: 10, second: 5, nanosecond: 230000000)),
-        (targetString: "-P2M4DT10M5.23S", expectedResult: DateComponents(month: -2, day: -4, minute: -10, second: -5, nanosecond: -230000000)),
-        (targetString: "PT0.001S", expectedResult: DateComponents(second: 0, nanosecond: 1000000)),
+        (targetString: "P2M4DT10M5.23S", expectedResult: DateComponents(month: 2, day: 4, minute: 10, second: 5, nanosecond: 2300000000)),
+        (targetString: "-P2M4DT10M5.23S", expectedResult: DateComponents(month: -2, day: -4, minute: -10, second: -5, nanosecond: -2300000000)),
+        (targetString: "PT0.001S", expectedResult: DateComponents(second: 0, nanosecond: 10000000)),
         (targetString: "-P2M", expectedResult: DateComponents(month: -2)),
         (targetString: "PT168H120M", expectedResult: DateComponents(hour: 168, minute: 120)),
         (targetString: "pt168h120m", expectedResult: DateComponents(hour: 168, minute: 120)),
-        (targetString: "Norman", expectedResult: nil),
         (targetString: "T168H120M", expectedResult: nil),
         (targetString: "P-20M", expectedResult: nil),
         (targetString: "P20MT", expectedResult: nil),
@@ -45,21 +45,88 @@ class RVS_ParseXMLDurationTests: XCTestCase {
         (targetString: "P15.5Y", expectedResult: nil),
         (targetString: "P1D2H", expectedResult: nil),
         (targetString: "1Y2M", expectedResult: nil),
-        (targetString: "P2M1Y", expectedResult: nil),
         (targetString: "P", expectedResult: nil),
-        (targetString: "PT15.S", expectedResult: nil),
-        (targetString: "", expectedResult: nil)
+        (targetString: "", expectedResult: nil),
+        (targetString: "Norman", expectedResult: nil),
+        (targetString: "P2M1Y", expectedResult: DateComponents(year: 1, month: 2)), // Even though this is illegal, we will still successfully parse it.
+        (targetString: "PT15.S", expectedResult: DateComponents(second: 15))        // Even though this is illegal, we will still successfully parse it.
     ]
     
-    func runTest(_ inTestTuple: TestTuple) {
-        if let testComponents = inTestTuple.targetString.asXMLDuration, let expectedResult = inTestTuple.expectedResult {
-            XCTAssertEqual(testComponents, expectedResult, "Unexpected result")
+    static let fromComponentsTests: [FromComponentsTestTuple] = [
+        (targetComponents: DateComponents(year: 1, month: 1, day: 1, hour: 1, minute: 1, second: 1, nanosecond: 1000000000), expectedResult: "P1Y1M1DT1H1M1.1S"),
+        (targetComponents: DateComponents(year: 1, month: 2, day: 3, hour: 4, minute: 5, second: 6, nanosecond: 7000000000), expectedResult: "P1Y2M3DT4H5M6.7S"),
+        (targetComponents: DateComponents(year: 1), expectedResult: "P1Y"),
+        (targetComponents: DateComponents(month: 1), expectedResult: "P1M"),
+        (targetComponents: DateComponents(day: 1), expectedResult: "P1D"),
+        (targetComponents: DateComponents(hour: 1), expectedResult: "PT1H"),
+        (targetComponents: DateComponents(minute: 1), expectedResult: "PT1M"),
+        (targetComponents: DateComponents(second: 1), expectedResult: "PT1S"),
+        (targetComponents: DateComponents(nanosecond: 1000000000), expectedResult: "PT0.1S"),
+        (targetComponents: DateComponents(second: 1, nanosecond: 1000000000), expectedResult: "PT1.1S"),
+        (targetComponents: DateComponents(year: 9999), expectedResult: "P9999Y"),
+        (targetComponents: DateComponents(month: 9999), expectedResult: "P9999M"),
+        (targetComponents: DateComponents(day: 9999), expectedResult: "P9999D"),
+        (targetComponents: DateComponents(hour: 9999), expectedResult: "PT9999H"),
+        (targetComponents: DateComponents(minute: 9999), expectedResult: "PT9999M"),
+        (targetComponents: DateComponents(second: 9999), expectedResult: "PT9999S"),
+        (targetComponents: DateComponents(nanosecond: 9999000000), expectedResult: "PT0.9999S"),
+        (targetComponents: DateComponents(second: 9999, nanosecond: 9999000000), expectedResult: "PT9999.9999S"),
+        (targetComponents: DateComponents(year: -1, month: -1, day: -1, hour: -1, minute: -1, second: -1, nanosecond: -1000000000), expectedResult: "-P1Y1M1DT1H1M1.1S"),
+        (targetComponents: DateComponents(year: 1, month: -1, day: -1, hour: -1, minute: -1, second: -1, nanosecond: -1000000000), expectedResult: nil),
+        (targetComponents: DateComponents(year: -1, month: 1, day: -1, hour: -1, minute: -1, second: -1, nanosecond: -1000000000), expectedResult: nil),
+        (targetComponents: DateComponents(year: -1, month: -1, day: 1, hour: -1, minute: -1, second: -1, nanosecond: -1000000000), expectedResult: nil),
+        (targetComponents: DateComponents(year: -1, month: -1, day: -1, hour: 1, minute: -1, second: -1, nanosecond: -1000000000), expectedResult: nil),
+        (targetComponents: DateComponents(year: -1, month: -1, day: -1, hour: -1, minute: 1, second: -1, nanosecond: -1000000000), expectedResult: nil),
+        (targetComponents: DateComponents(year: -1, month: -1, day: -1, hour: -1, minute: -1, second: 1, nanosecond: -1000000000), expectedResult: nil),
+        (targetComponents: DateComponents(year: -1, month: -1, day: -1, hour: -1, minute: -1, second: -1, nanosecond: 1000000000), expectedResult: nil),
+        (targetComponents: DateComponents(), expectedResult: nil),
+        (targetComponents: DateComponents(year: -1), expectedResult: "-P1Y"),
+        (targetComponents: DateComponents(month: -1), expectedResult: "-P1M"),
+        (targetComponents: DateComponents(day: -1), expectedResult: "-P1D"),
+        (targetComponents: DateComponents(hour: -1), expectedResult: "-PT1H"),
+        (targetComponents: DateComponents(minute: -1), expectedResult: "-PT1M"),
+        (targetComponents: DateComponents(second: -1), expectedResult: "-PT1S"),
+        (targetComponents: DateComponents(nanosecond: -1000000000), expectedResult: "-PT0.1S"),
+        (targetComponents: DateComponents(second: -1, nanosecond: -1000000000), expectedResult: "-PT1.1S"),
+        (targetComponents: DateComponents(year: 1, month: 0, day: 0, hour: 0, minute: 0, second: 0, nanosecond: 0), expectedResult: "P1Y"),
+        (targetComponents: DateComponents(year: 0, month: 1, day: 0, hour: 0, minute: 0, second: 0, nanosecond: 0), expectedResult: "P1M"),
+        (targetComponents: DateComponents(year: 0, month: 0, day: 1, hour: 0, minute: 0, second: 0, nanosecond: 0), expectedResult: "P1D"),
+        (targetComponents: DateComponents(year: 0, month: 0, day: 0, hour: 1, minute: 0, second: 0, nanosecond: 0), expectedResult: "PT1H"),
+        (targetComponents: DateComponents(year: 0, month: 0, day: 0, hour: 0, minute: 1, second: 0, nanosecond: 0), expectedResult: "PT1M"),
+        (targetComponents: DateComponents(year: 0, month: 0, day: 0, hour: 0, minute: 0, second: 1, nanosecond: 0), expectedResult: "PT1S"),
+        (targetComponents: DateComponents(year: 0, month: 0, day: 0, hour: 0, minute: 0, second: 0, nanosecond: 1000000000), expectedResult: "PT0.1S"),
+        (targetComponents: DateComponents(year: 0, month: 0, day: 0, hour: 0, minute: 0, second: 1, nanosecond: 1000000000), expectedResult: "PT1.1S")
+    ]
+    
+    func runFromStringTest(_ inTestTuple: FromStringTestTuple) {
+        if let testComponents = inTestTuple.targetString.asXMLDuration {
+            if let expectedResult = inTestTuple.expectedResult {
+                XCTAssertEqual(testComponents, expectedResult, "Unexpected result")
+            } else {
+                XCTAssertNil(testComponents, "Should be nil!")
+            }
+        } else if nil != inTestTuple.expectedResult {
+            XCTFail("Not sure what happened here (testing \(String(describing: inTestTuple)))!")
         }
     }
     
+    func runFromComponentsTest(_ inTestTuple: FromComponentsTestTuple) {
+        if let testComponents = inTestTuple.targetComponents.asXMLDuration {
+            if let expectedResult = inTestTuple.expectedResult {
+                XCTAssertEqual(testComponents, expectedResult, "Unexpected result")
+            } else {
+                XCTAssertNil(testComponents, "Should be nil!")
+            }
+        }
+    }
+
     func testBasics() {
-        type(of: self).testSequence.forEach {
-            runTest($0)
+        type(of: self).fromStringTests.forEach {
+            runFromStringTest($0)
+        }
+
+        type(of: self).fromComponentsTests.forEach {
+            runFromComponentsTest($0)
         }
     }
 }
